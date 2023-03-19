bb735dd38fba3fa2da3cd801063c9103fdd948e0
Cody Boring <codyboring94@gmail.com>
2023-03-19 15:59:03 -0700

    updated assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.textContent = "is the champion";
+newHeader.id = 'victory';
+document.head.append(newHeader);
+const removeMain = document.getElementById("main");
+removeMain.remove();
+
+
